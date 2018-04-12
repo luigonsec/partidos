633dd59a84297b024dacbcff076159e90b8e5c8f
Luis Gonzalez <luisgonzalezseco@gmail.com>
2018-04-12 22:57:15 +0200

    V2

--- server.js
+++ server.js
@@ -7,7 +7,7 @@
 const express = require('express') // call express
 const app = express() // define our app using express
 const bodyParser = require('body-parser')
-
+const configLocal = require('./config/local.js')
 const DatabaseService = require('./services/DatabaseService')
 DatabaseService.iniciar()
 
@@ -15,19 +15,23 @@ DatabaseService.iniciar()
 const UsersRoutes = require('./routes/users')
 const MatchesRoutes = require('./routes/matches')
 const TeamsRoutes = require('./routes/teams')
+const cors = require('cors')
+
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(cors({
+  origin: 'http://localhost:8080'
+}))
 
 
 UsersRoutes(app)
 MatchesRoutes(app)
 TeamsRoutes(app)
 
-var port = process.env.PORT || 8080
+var port = process.env.PORT || configLocal.port || 8081
 app.listen(port)
-
 
 module.exports = app
